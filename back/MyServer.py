ac17a62b571ac1403b1953bba7ef4421788e6e3c
untitledC20H12 <95670812+C20H12@users.noreply.github.com>
2023-04-14 00:19:45 -0600

    removed logout, sanitizer

--- back/MyServer.py
+++ back/MyServer.py
@@ -15,16 +15,6 @@ filesMap = {fileName: filePath for fileName, filePath in zip(frontEndFileNames, 
 
 class MyServer(BaseHTTPRequestHandler):
   
-  def respond(self, code: int, headers: dict, content: Union[bytes, str]):
-    self.send_response(code)
-    for key, value in headers.items():
-      self.send_header(key, value)
-    self.end_headers()
-    if type(content) is str:
-      self.wfile.write(content.encode("utf-8"))
-    else:
-      self.wfile.write(content)
-
   def readFile(self, filePath: str):
     with open(filePath, 'rb') as file:
       return file.read()
@@ -34,6 +24,18 @@ class MyServer(BaseHTTPRequestHandler):
   
   def getPostContent(self):
     return self.rfile.read(int(self.headers.get("Content-Length"))).decode('utf-8')
+  
+
+  # responding methods
+  def respond(self, code: int, headers: dict, content: Union[bytes, str]):
+    self.send_response(code)
+    for key, value in headers.items():
+      self.send_header(key, value)
+    self.end_headers()
+    if type(content) is str:
+      self.wfile.write(content.encode("utf-8"))
+    else:
+      self.wfile.write(content)
 
   def sendNotFound(self):
     self.respond(404, {"Content-type": "text/html"}, "<h1>404 Not Found</h1>")
@@ -51,6 +53,13 @@ class MyServer(BaseHTTPRequestHandler):
   def sendSuccess(self, msg: str, data=[]):
     self.respond(200, {"Content-type": "text/json"}, JSON_stringify({"message": msg, "data": data}))
 
+  def sendError(self, msg: str):
+    self.respond(500, {"Content-type": "text/json"}, JSON_stringify({"message": msg}))
+
+  def sanitizeInput(self, inputStr: str):
+    return inputStr.replace("<", "&lt;").replace(">", "&gt;")
+
+  # listening methods
   def do_GET(self):
     if self.getStrippedPath() == '':
       self.respond(200, {"Content-type": "text/html"}, self.readFile(filesMap['index.html']))
@@ -71,7 +80,9 @@ class MyServer(BaseHTTPRequestHandler):
     data = recieved['data']
     accountInfo = recieved['accountInfo']
 
-    account = Account(accountInfo['username'], accountInfo['password'])
+    account = Account(
+      self.sanitizeInput(accountInfo['username']), self.sanitizeInput(accountInfo['password'])
+    )
 
     if action == "login":
       if not account.exists:
@@ -102,9 +113,6 @@ class MyServer(BaseHTTPRequestHandler):
         account.transferTo(Account(data['transfer-target'], asTransferTarget=True), float(data['transfer-amount']))
         self.sendSuccess("Transfer succesful", account.getTransactions())
 
-      if action == "logout":
-        self.sendSuccess("Logged out")
-
       if action == "change-pass":
         account.changePassword(data['change-password-new'])
         self.sendSuccess("Password changed")
@@ -112,10 +120,12 @@ class MyServer(BaseHTTPRequestHandler):
       if action == "delete":
         account.delete(data['delete-password-confirm'])
         self.sendSuccess("Account deleted")
-    
+
+      # logout is handled by the front end
+
     except AccountError as ae:
       self.sendUnauthorized(f"Account Error: {ae}")
       return
     
-    except Exception as ex:
-      self.respond(500, {"Content-type": "text/json"}, f"Internal server error: {ex}")
+    # except Exception as ex:
+    #   self.sendError(f"Internal Error: {ex}")
